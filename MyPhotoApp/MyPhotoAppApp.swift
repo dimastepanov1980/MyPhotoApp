ada9fdd506b162ac8fed5dd03bd32eb26c8572e4
dimastepanov <dimastepanov1980@gmail.com>
2023-05-17 12:41:25 +0700

    Initial Commit

---
+++ MyPhotoApp/MyPhotoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPhotoAppApp.swift
+//  MyPhotoApp
+//
+//  Created by Dima Stepanov on 5/17/23.
+//
+
+import SwiftUI
+
+@main
+struct MyPhotoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
