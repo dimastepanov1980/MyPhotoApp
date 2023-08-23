66a3e04f78194000ad692430184221da729387c4
dimastepanov <dimastepanov1980@gmail.com>
2023-08-23 21:22:16 +0700

    func get all portfolio

--- MyPhotoApp/Sources/BusinessLogicLayer/Services/Firesore/UserManager.swift
+++ MyPhotoApp/Sources/BusinessLogicLayer/Services/Firesore/UserManager.swift
@@ -13,20 +13,10 @@ import Combine
 final class UserManager {
     
     static let shared = UserManager()
+    
+    //MARK: - Author Section
     private let userCollection = Firestore.firestore().collection("users")
     private var listenerRegistration: ListenerRegistration?
-    private let encoder: Firestore.Encoder = {
-        let encoder = Firestore.Encoder()
-//        encoder.keyEncodingStrategy = .convertToSnakeCase
-        return encoder
-    }()
-    private let decoder: Firestore.Decoder = {
-        let decoder = Firestore.Decoder()
-//        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        return decoder
-    }()
-
-    init() {}
     
     private func userDocument(userId: String) -> DocumentReference {
         userCollection.document(userId)
@@ -133,10 +123,26 @@ final class UserManager {
     func getUser(userId: String) async throws -> DBUserModel {
         try await userDocument(userId: userId).getDocument(as: DBUserModel.self)
     }
+    
+    
+    //MARK: - Customer Section
+    
+    private let portfolioCollection = Firestore.firestore().collection("portfolio")
+    
+    private func authorLocation(location: String) -> DocumentReference {
+        portfolioCollection.document(location)
+    }
+    private func authorsPortfolionOnLocation(location: String) -> CollectionReference {
+        authorLocation(location: location).collection("author")
+    }
+
+    func getAllPortfolio(location: String) async throws -> [DBPortfolioModel] {
+        try await authorsPortfolionOnLocation(location: location).getDocuments(as: DBPortfolioModel.self)
+    }
+    
 }
 
 extension Query {
-    
     func getDocuments<T>(as type: T.Type) async throws -> [T] where T : Decodable {
         try await getDocumentsWithSnapshot(as: type).products
     }
