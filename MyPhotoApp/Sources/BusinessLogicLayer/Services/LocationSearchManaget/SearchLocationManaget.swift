aa7bb84fbf464b5fdf0c47e75882f32739fbe9aa
dimastepanov <dimastepanov1980@gmail.com>
2023-09-22 09:09:07 +0700

    get current location but 2 times

--- MyPhotoApp/Sources/BusinessLogicLayer/Services/LocationSearchManaget/SearchLocationManaget.swift
+++ MyPhotoApp/Sources/BusinessLogicLayer/Services/LocationSearchManaget/SearchLocationManaget.swift
@@ -9,8 +9,77 @@ import Foundation
 import MapKit
 import Combine
 
-//@MainActor
-final class SearchLocationManager {
+@MainActor
+final class SearchLocationManager: NSObject, CLLocationManagerDelegate {
+    let searchLocationPublisher = PassthroughSubject<[MKMapItem], Never>()
+    private var locationManager = CLLocationManager()
+    private let center: CLLocationCoordinate2D
+    private let radius: CLLocationDistance
+    private var currentCoordinate: CLLocationCoordinate2D?
+    private var searchTimer: Timer?
+    private let searchDelay: TimeInterval = 0.7 // Adjust the delay time as needed
+    
+    init(in center: CLLocationCoordinate2D, radius: CLLocationDistance = .infinity) {
+        self.center = center
+        self.radius = radius
+        super.init()
+        self.locationManager.delegate = self
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+    }
+    
+    public func searchLocation(searchText: String) {
+        // Invalidate the previous timer, if any
+        searchTimer?.invalidate()
+        
+        // Start a new timer
+        searchTimer = Timer.scheduledTimer(withTimeInterval: searchDelay, repeats: false) { [weak self] _ in
+            self?.requestLocation(resultType: [.pointOfInterest, .address], searchText: searchText)
+        }
+    }
+    
+    func updateLocation(_ coordinate: CLLocationCoordinate2D) {
+        currentCoordinate = coordinate
+    }
+    
+    private func requestLocation(resultType: MKLocalSearch.ResultType = .address, searchText: String) {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = searchText
+        request.pointOfInterestFilter = .includingAll
+        request.resultTypes = resultType
+        request.region = MKCoordinateRegion(center: center,
+                                           latitudinalMeters: radius,
+                                           longitudinalMeters: radius)
+        let search = MKLocalSearch(request: request)
+        
+        search.start { [weak self](response, _) in
+            guard let response = response else {
+                return
+            }
+            self?.searchLocationPublisher.send(response.mapItems)
+        }
+    }
+    
+    func requestCurrentLocation() {
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+
+    func getCurrentLocation() -> CLLocationCoordinate2D? {
+        return locationManager.location?.coordinate
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        updateLocation(location.coordinate)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Location manager failed with error: \(error.localizedDescription)")
+    }
+}
+
+
+ final class SearchLocationManager1 {
     
     let searchLocationPublisher = PassthroughSubject<[MKMapItem], Never>()
     private let center: CLLocationCoordinate2D
@@ -57,6 +126,7 @@ final class SearchLocationManager {
         }
     }
 }
+ 
 
 
 
--- MyPhotoApp/Sources/CustomerUserStory/CustomerMainScreenUserStory/ViewModels/CustomerMainScreenViewModel.swift
+++ MyPhotoApp/Sources/CustomerUserStory/CustomerMainScreenUserStory/ViewModels/CustomerMainScreenViewModel.swift
@@ -13,12 +13,10 @@ import PhotosUI
 
 @MainActor
 final class CustomerMainScreenViewModel: CustomerMainScreenViewModelType, ObservableObject {
-    var curentLocation = LocationService()
 
     var searchService: SearchLocationManager
-
     private var cancellable: AnyCancellable?
-    private var cancellableLocation = Set<AnyCancellable>()
+    private var hasHandledLocationResult = false
     
     @Published var imagesURLs: [URL]? = nil
     @Published var portfolio: [AuthorPortfolioModel] = []
@@ -36,57 +34,32 @@ final class CustomerMainScreenViewModel: CustomerMainScreenViewModelType, Observ
     
     init() {
         searchService = SearchLocationManager(in: CLLocationCoordinate2D())
-        cancellable = searchService.searchLocationPublisher.sink { mapItems in
-            self.locationResult = mapItems.map({ DBLocationModel(mapItem: $0) })
+        cancellable = searchService.searchLocationPublisher.sink { [weak self] mapItems in
+            guard !(self?.hasHandledLocationResult ?? true) else { return }
+            self?.locationResult = mapItems.map({ DBLocationModel(mapItem: $0) })
         }
-        curentLocation.$location
-            .receive(on: DispatchQueue.main) // Ensure updates are received on the main thread
-            .sink { [weak self] newLocation in
-                // Update the view with the new location value
-                self?.handleNewLocation(newLocation)
-            }
-            .store(in: &cancellableLocation)
         
-        Task {
-            try await fetchLocation()
-        }
-        }
-    
-    func fetchLocation() async throws {
-        try await curentLocation.requestLocation()
+        getPortfolioForCurrentLocation()
     }
     
-    private func handleNewLocation(_ location: CLLocation?) {
-        guard let location = location else {
-            print("Location is nil.")
-            return
-        }
-        fetchPortfolioForCurrentLocation(with: location)
-    }
-    func fetchPortfolioForCurrentLocation(with location: CLLocation) {
-        let longitude = location.coordinate.longitude.binade
-        let latitude = location.coordinate.latitude.binade
-        print(longitude, latitude)
-
+    private func getPortfolioForCurrentLocation() {
+        searchService.requestCurrentLocation()
+               if let currentLocation = searchService.getCurrentLocation() {
+                   self.latitude = currentLocation.latitude
+                   self.longitude = currentLocation.longitude
+                   print(latitude, longitude)
+               }
         Task{
             do {
                 let dbPortfolio = try await getPortfolio(longitude: longitude, latitude: latitude)
                 self.portfolio = dbPortfolio.map { AuthorPortfolioModel(portfolio: $0) }
-                print(portfolio)
-                print(location.coordinate.longitude.binade, location.coordinate.latitude.binade)
+                print("dbPortfolio: \(dbPortfolio)")
             } catch {
                 print(String(describing: error))
             }
         }
     }
-    func getPortfolioForLocation(location: String) async throws -> [DBPortfolioModel] {
-        do {
-            let portfolio = try await UserManager.shared.getPortfolioLocation(location: location)
-            return portfolio
-        } catch {
-            throw error
-        }
-    }
+    
     func getPortfolio(longitude: Double , latitude: Double) async throws -> [DBPortfolioModel] {
         do {
             let portfolio = try await UserManager.shared.getPortfolioCoordinateRange(longitude: longitude, latitude: latitude)
@@ -114,3 +87,4 @@ final class CustomerMainScreenViewModel: CustomerMainScreenViewModelType, Observ
         searchService.searchLocation(searchText: text)
     }
 }
+
